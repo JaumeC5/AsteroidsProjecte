24f96711d09c3921f05a72bdc4238a952505b37c
Kibium <toniferrari87@gmail.com>
2017-01-02 18:35:56 +0100

    asdf
    SAD

--- src/Snake/GameEngine.hh
+++ src/Snake/GameEngine.hh
@@ -141,7 +141,7 @@ void Run() {
 			SDL_RenderCopy(R.getRender(), exit.convertSurface(R.getRender()), nullptr, &exit.getRect());
 			SDL_RenderCopy(R.getRender(), title.convertSurface(R.getRender()), nullptr, &title.getRect());
 			//
-
+			//
 			if (IM.onButton(play.getRect()))
 				scene = 2;
 			else if (IM.onButton(exit.getRect())) {
